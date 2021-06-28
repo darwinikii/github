667b4ec3702d6bc4b0297e659e12cd25fc9fa905
darwinikii <74075511+darwinikii@users.noreply.github.com>
2021-06-28 18:59:56 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -2,3 +2,11 @@ if game.GameId == 2429242760 then
     loadstring(game:HttpGet(('https://github.com/darwinikii/github/raw/main/blxyn.lua'), true))()
     return
 end
+if game.GameId == 111311599 then
+    loadstring(game:HttpGet(('https://github.com/darwinikii/github/raw/main/critst.lua'), true))()
+    return
+end
+if game.GameId == 4520749081 then
+    loadstring(game:HttpGet(('https://github.com/darwinikii/github/raw/main/kngp.lua'), true))()
+    return
+end
