880fce2a5676cc68a520115df819f769601fe6fb
darwinikii <74075511+darwinikii@users.noreply.github.com>
2021-06-28 18:57:23 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,4 +1,4 @@
 if game.GameId == 2429242760 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/RandomAdamYT/DarkHub/master/BladeQuest'), true))()
+    loadstring(game:HttpGet(('https://github.com/darwinikii/github/raw/main/blxyn.lua'), true))()
     return
 end
